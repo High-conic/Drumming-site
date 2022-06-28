fb6aa6ebb25a5f36bc9d5fcb4787e6109ea831a7
Timothy Akintemi <akintemitimothy@yahoo.com>
2022-06-28 14:28:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+//Detecting Button Press
+
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var n = 0; n < noOfDrumButtons; n++){
+    
+    document.querySelectorAll(".drum")[n].addEventListener("click", function(){
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    });
+}
+
+//Detecting Keyboard Press
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3"); 
+            tom1.play();
+            break;
+        
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3"); 
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3"); 
+            tom3.play();
+            break;
+
+        case "d":
+            var kick = new Audio("sounds/kick-bass.mp3"); 
+            kick.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3"); 
+            snare.play();
+            break;
+
+        case "k":
+            var tom4 = new Audio("sounds/tom-4.mp3"); 
+            tom4.play();
+            break;
+
+        case "l":
+            var crash = new Audio("sounds/crash.mp3"); 
+            crash.play();
+            break;
+ 
+            default: console.log(buttonInnerHTML); 
+    }
+}
+
+function buttonAnimation(currentkey){
+    
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 200);
+}
